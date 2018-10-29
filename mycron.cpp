0053bf984759234b429f7f9e725d167b05d9ae5f
Kiseliov <dmitrykiseliov2000@gmail.com>
2018-10-29 23:24:09 +0300

    tries to do read_file

--- mycron.cpp
+++ mycron.cpp
@@ -9,14 +9,23 @@ struct task{
     int hour;
     int min;
     int sec;
-    vector<string>targets;
+    string task_roll;
 }
 
 vector<task>Tasks;
 
 void read_file(){
 	//function that read and prepare data from input file for use
-	//TODO 
+	FILE *f;
+	f = fopen("mycrontab", "r");
+	string temp_str;
+	char* temp_Cstring;
+	struct task temp_task;
+	while(getline(f, temp_str) > 0){
+		sscanf(temp_str, "%d:%d:%d %s", &temp_task.hour, &temp_task.min, &temp_task.sec, temp_task.task_roll);	//Now we have one of tasks in temp_task
+
+	}
+	printf("%s\n", temp_str);
 }
 
 void changed(){ //function that will look for changes in input file
@@ -28,14 +37,16 @@ void update_tasks(){ //function that will update vector of tasks
 }
 
 int main(){
-    for(;;){	// main loop 
+   /* for(;;){	// main loop 
 	if(changed){
 	    update_tasks();
 	}
 	for(int i = 0; i < Tasks.size(); i++){
 	    if(current_Time >= Tasks[i].Time) exec();
 	}
-    }
+    }*/
+    read_file();
+
 }
 /*
     time_t t = time(0);
